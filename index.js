4f5821c58fbbc88e5aace2fee4f156bfae07d8e9
Koon <gameskoon101020@gmail.com>
2023-06-15 05:47:56 +0700

    update version 1.0.6

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+ 
+app.use(express.static('public'))
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/static/index.html'));
+});
+router.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/static/about.html'));
+});
+router.get('/contact',function(req,res){
+  res.sendFile(path.join(__dirname+'/static/contact.html'));
+});
+router.get('/news',function(req,res){
+  res.sendFile(path.join(__dirname+'/static/news.html'));
+});
+router.get('/socialmedia',function(req,res){
+  res.sendFile(path.join(__dirname+'/static/socialmedia.html'));
+});
+
+app.use('/', router);
+app.listen(4000);
+console.log('Running at Port 4000');
