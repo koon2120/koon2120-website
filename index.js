676478113716c1e4dcb4cb33e93d48710d3aafb5
Koon <gameskoon101020@gmail.com>
2023-06-15 06:13:12 +0700

    update version 1.0.8

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
- 
-app.use('/static', express.static('public'))
 
 router.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/static/index.html'));
@@ -19,6 +17,25 @@ router.get('/news',function(req,res){
 });
 router.get('/socialmedia',function(req,res){
   res.sendFile(path.join(__dirname+'/static/socialmedia.html'));
+});
+
+router.get('/css/commingsoon',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/styles/comingsoon.css'));
+});
+router.get('/css/index',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/styles/index.css'));
+});
+router.get('/scrpit',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/script.js'));
+});
+router.get('/logo_favicon',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/content/logo_favicon.png'));
+});
+router.get('/logo',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/content/logo.png'));
+});
+router.get('/video_bg',function(req,res){
+  res.sendFile(path.join(__dirname+'/public/content/video_bg.mp4'));
 });
 
 app.use('/', router);
