ca3769f0d375f8a45099d289b29a8d78b89579f7
Koon <gameskoon101020@gmail.com>
2023-09-21 17:10:40 +0700

    1.2.2

--- index.js
+++ index.js
@@ -10,30 +10,30 @@ app.use('/static', express.static(path.join(__dirname, 'static')))
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.render('components/main',{
-    "icon":"https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
-    "title":"Koon2120 Official Website",
-    "description":"welcome to koon2120 official website!!",
-    "keywords":"koon2120,koon,koon oppe,koon kutsai",
-    "url":req.protocol + "://" + req.hostname + "/",
-    "site_name":"Koon2120",
-    "og_image":"https://api.koon2120.online/koon2120-website/asset/cover-share.png",
-    "style_link":"/static/styles/index.css",
-    "page":"../pages/index"
+  res.render('components/main', {
+    "icon": "https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
+    "title": "Koon2120 Official Website",
+    "description": "welcome to koon2120 official website!!",
+    "keywords": "koon2120,koon,koon oppe,koon kutsai",
+    "url": req.protocol + "://" + req.hostname + "/",
+    "site_name": "Koon2120",
+    "og_image": "https://api.koon2120.online/koon2120-website/asset/cover-share.png",
+    "style_link": "/static/styles/index.css",
+    "page": "../pages/index"
   })
 })
 
 app.get('/about', (req, res) => {
-  res.render('components/main',{
-    "icon":"https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
-    "title":"About - Koon2120 Official Website",
-    "description":"welcome to koon2120 official website!!",
-    "keywords":"koon2120,koon,koon oppe,koon kutsai",
-    "url":req.protocol + "://" + req.hostname + "/",
-    "site_name":"Koon2120",
-    "og_image":"https://api.koon2120.online/koon2120-website/asset/cover-share.png",
-    "style_link":"/static/styles/global.css",
-    "page":"../pages/global"
+  res.render('components/main', {
+    "icon": "https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
+    "title": "About - Koon2120 Official Website",
+    "description": "welcome to koon2120 official website!!",
+    "keywords": "koon2120,koon,koon oppe,koon kutsai",
+    "url": req.protocol + "://" + req.hostname + "/",
+    "site_name": "Koon2120",
+    "og_image": "https://api.koon2120.online/koon2120-website/asset/cover-share.png",
+    "style_link": "/static/styles/about.css",
+    "page": "../pages/about"
   })
 })
 
@@ -46,99 +46,103 @@ app.get('/blog', (req, res) => {
 })
 
 app.get('/projects', (req, res) => {
-  res.render('components/main',{
-    "icon":"https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
-    "title":"Projects - Koon2120 Official Website",
-    "description":"welcome to koon2120 official website!!",
-    "keywords":"koon2120,koon,koon oppe,koon kutsai",
-    "url":req.protocol + "://" + req.hostname + "/",
-    "site_name":"Koon2120",
-    "og_image":"https://api.koon2120.online/koon2120-website/asset/cover-share.png",
-    "style_link":"/static/styles/global.css",
-    "page":"../pages/global"
+  res.render('components/main', {
+    "icon": "https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
+    "title": "Projects - Koon2120 Official Website",
+    "description": "welcome to koon2120 official website!!",
+    "keywords": "koon2120,koon,koon oppe,koon kutsai",
+    "url": req.protocol + "://" + req.hostname + "/",
+    "site_name": "Koon2120",
+    "og_image": "https://api.koon2120.online/koon2120-website/asset/cover-share.png",
+    "style_link": "/static/styles/projects.css",
+    "page": "../pages/projects"
   })
 })
 
 app.get('/follow_us', (req, res) => {
-  res.render('components/main',{
-    "icon":"https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
-    "title":"Follow Us - Koon2120 Official Website",
-    "description":"welcome to koon2120 official website!!",
-    "keywords":"koon2120,koon,koon oppe,koon kutsai",
-    "url":req.protocol + "://" + req.hostname + "/",
-    "site_name":"Koon2120",
-    "og_image":"https://api.koon2120.online/koon2120-website/asset/cover-share.png",
-    "style_link":"/static/styles/global.css",
-    "page":"../pages/global"
+  res.render('components/main', {
+    "icon": "https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
+    "title": "Follow Us - Koon2120 Official Website",
+    "description": "welcome to koon2120 official website!!",
+    "keywords": "koon2120,koon,koon oppe,koon kutsai",
+    "url": req.protocol + "://" + req.hostname + "/",
+    "site_name": "Koon2120",
+    "og_image": "https://api.koon2120.online/koon2120-website/asset/cover-share.png",
+    "style_link": "/static/styles/follow_us.css",
+    "page": "../pages/follow_us"
   })
 })
 
 app.get('/contact_us', (req, res) => {
-  res.render('components/main',{
-    "icon":"https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
-    "title":"Contact Us - Koon2120 Official Website",
-    "description":"welcome to koon2120 official website!!",
-    "keywords":"koon2120,koon,koon oppe,koon kutsai",
-    "url":req.protocol + "://" + req.hostname + "/",
-    "site_name":"Koon2120",
-    "og_image":"https://api.koon2120.online/koon2120-website/asset/cover-share.png",
-    "style_link":"/static/styles/global.css",
-    "page":"../pages/global"
+  res.render('components/main', {
+    "icon": "https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
+    "title": "Contact Us - Koon2120 Official Website",
+    "description": "welcome to koon2120 official website!!",
+    "keywords": "koon2120,koon,koon oppe,koon kutsai",
+    "url": req.protocol + "://" + req.hostname + "/",
+    "site_name": "Koon2120",
+    "og_image": "https://api.koon2120.online/koon2120-website/asset/cover-share.png",
+    "style_link": "/static/styles/contact_us.css",
+    "page": "../pages/contact_us"
   })
 })
 
 app.get('/privacy_policy', (req, res) => {
-  res.render('components/main',{
-    "icon":"https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
-    "title":"Privacy Policy - Koon2120 Official Website",
-    "description":"welcome to koon2120 official website!!",
-    "keywords":"koon2120,koon,koon oppe,koon kutsai",
-    "url":req.protocol + "://" + req.hostname + "/",
-    "site_name":"Koon2120",
-    "og_image":"https://api.koon2120.online/koon2120-website/asset/cover-share.png",
-    "style_link":"/static/styles/global.css",
-    "page":"../pages/global"
+  res.render('components/main', {
+    "icon": "https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
+    "title": "Privacy Policy - Koon2120 Official Website",
+    "description": "welcome to koon2120 official website!!",
+    "keywords": "koon2120,koon,koon oppe,koon kutsai",
+    "url": req.protocol + "://" + req.hostname + "/",
+    "site_name": "Koon2120",
+    "og_image": "https://api.koon2120.online/koon2120-website/asset/cover-share.png",
+    "style_link": "/static/styles/global.css",
+    "page": "../pages/global"
   })
 })
 
 app.get('/term_of_service', (req, res) => {
-  res.render('components/main',{
-    "icon":"https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
-    "title":"Term Of Service - Koon2120 Official Website",
-    "description":"welcome to koon2120 official website!!",
-    "keywords":"koon2120,koon,koon oppe,koon kutsai",
-    "url":req.protocol + "://" + req.hostname + "/",
-    "site_name":"Koon2120",
-    "og_image":"https://api.koon2120.online/koon2120-website/asset/cover-share.png",
-    "style_link":"/static/styles/global.css",
-    "page":"../pages/global"
+  res.render('components/main', {
+    "icon": "https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
+    "title": "Term Of Service - Koon2120 Official Website",
+    "description": "welcome to koon2120 official website!!",
+    "keywords": "koon2120,koon,koon oppe,koon kutsai",
+    "url": req.protocol + "://" + req.hostname + "/",
+    "site_name": "Koon2120",
+    "og_image": "https://api.koon2120.online/koon2120-website/asset/cover-share.png",
+    "style_link": "/static/styles/global.css",
+    "page": "../pages/global"
   })
 })
 
 app.get('/cookie_policy', (req, res) => {
-  res.render('components/main',{
-    "icon":"https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
-    "title":"Cookie Policy - Koon2120 Official Website",
-    "description":"welcome to koon2120 official website!!",
-    "keywords":"koon2120,koon,koon oppe,koon kutsai",
-    "url":req.protocol + "://" + req.hostname + "/",
-    "site_name":"Koon2120",
-    "og_image":"https://api.koon2120.online/koon2120-website/asset/cover-share.png",
-    "style_link":"/static/styles/global.css",
-    "page":"../pages/global"
+  res.render('components/main', {
+    "icon": "https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
+    "title": "Cookie Policy - Koon2120 Official Website",
+    "description": "welcome to koon2120 official website!!",
+    "keywords": "koon2120,koon,koon oppe,koon kutsai",
+    "url": req.protocol + "://" + req.hostname + "/",
+    "site_name": "Koon2120",
+    "og_image": "https://api.koon2120.online/koon2120-website/asset/cover-share.png",
+    "style_link": "/static/styles/global.css",
+    "page": "../pages/global"
   })
 })
 
+app.get('/static/images/bg/random', (req, res) => {
+  res.sendFile(path.join(__dirname, `/static/images/bg/${Math.floor((Math.random() * 16) + 1)}.png`))
+})
+
 app.get('/*', (req, res) => {
-  res.render('pages/not_found',{
-    "icon":"https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
-    "title":"Not Found - Koon2120 Official Website",
-    "description":"welcome to koon2120 official website!!",
-    "keywords":"koon2120,koon,koon oppe,koon kutsai",
-    "url":req.protocol + "://" + req.hostname + "/",
-    "site_name":"Koon2120",
-    "og_image":"https://api.koon2120.online/koon2120-website/asset/cover-share.png",
-    "style_link":"/static/styles/not_found.css"
+  res.render('pages/not_found', {
+    "icon": "https://api.koon2120.online/koon2120-website/asset/logo_favicon.ico",
+    "title": "Not Found - Koon2120 Official Website",
+    "description": "welcome to koon2120 official website!!",
+    "keywords": "koon2120,koon,koon oppe,koon kutsai",
+    "url": req.protocol + "://" + req.hostname + "/",
+    "site_name": "Koon2120",
+    "og_image": "https://api.koon2120.online/koon2120-website/asset/cover-share.png",
+    "style_link": "/static/styles/not_found.css"
   })
 })
 
